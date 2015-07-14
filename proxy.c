b927e7bb1e39a24ee6d6a96153c20281df3d35ea
Jarvishappy <jarvisHappyHacking@gmail.com>
2015-07-14 15:42:41 +0800

    [Thread per connection] initial framework

--- proxy.c
+++ proxy.c
@@ -10,12 +10,17 @@
  * function that describes what that function does.
  */
 
+#include <assert.h>
 #include <execinfo.h>
 #include <signal.h>
 #include "csapp.h"
 #define DBG(x) do { printf("%s:%d - ", __func__, __LINE__); printf x; \
     putchar('\n'); fflush(stdout); } while(0);
 
+#define LOG_ERROR(x) do { printf("ERROR: %s:%d - ", __func__, __LINE__); printf x; \
+    putchar('\n'); fflush(stdout); } while(0);
+
+#define MAXTHREAD 2
 #define MAXHEAD 30
 #define CRLF "\r\n"
 #define CONTENT_LENGTH "Content-Length"
@@ -27,21 +32,39 @@
  * 读完request line之后就有足够的参数connect到target server了，
  * 然后就可以读一行写一行，这样能节省很多内存
  */
-typedef struct {
-    int content_len;
+typedef struct http_reqline {
     int port;
     char method[5];
+    char versionCRLF[10];
     char host[128];
     char path[1024];
     char uri[MAXLINE];
-} reqmeta_t;
+} http_reqline_t;
 
-typedef struct {
+typedef struct io_buf {
     char linebuf[MAXLINE];
     char bodybuf[MAXBUF];
 } io_buf_t;
 
-void handler(int sig) {
+typedef struct proxy_logger {
+    FILE *fp;
+    sem_t mutex;
+} proxy_logger_t;
+
+
+typedef void*(thread_func_t)(void*);
+
+/* pthread_create()传给thread的参数 */
+typedef struct thread_context {
+    int connfd; /* connection fd */
+    int tid;    /* thread id */
+    struct sockaddr_in conn_addr;
+    proxy_logger_t *logger;
+
+} thread_context_t;
+
+
+void segv_handler(int sig) {
     void *array[10];
     size_t size;
 
@@ -57,66 +80,34 @@ void handler(int sig) {
 /*
  * Function prototypes
  */
+void print_err(int err, char *msg);
+proxy_logger_t *proxy_logger(const char *filename);
+void dolog(proxy_logger_t *loggerp, char *logstring);
+void free_proxy_logger(proxy_logger_t *loggerp);
+void free_thread_context(thread_context_t *thread_ctxp);
+void print_err(int err, char *msg);
+
+/** HTTP message parsing funcitons **/
 int parse_uri(char *uri, char *target_addr, char *path, int *port);
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
-void write_log(FILE *fp, char *logstring);
-
-/**
- * 解析HTTP Request的第一行，获取目的server的host和port
- */
-int parse_request_line(rio_t *rio_buf, reqmeta_t *reqmeta, io_buf_t *io_buf)
-{
-    if (Rio_readlineb(rio_buf, io_buf->linebuf, MAXLINE) == 0)
-        return 0;
-
-    /* check if a valid request line */
-    if (strstr(io_buf->linebuf, "HTTP") == NULL)
-        return -1;
-
-    char skipbuf[64];
-    sscanf(io_buf->linebuf, "%s %s %s", reqmeta->method, reqmeta->uri, skipbuf);
-
-/*     char *p = io_buf->linebuf;
- *     char *delim;
- * 
- *     delim = strchr(p, ' ');
- *     if (!delim)
- *         return -1;
- * 
- *     *delim = '\0';
- *     [> p points to the method <]
- *     strncpy(reqmeta->method, p, strlen(p) + 1);
- * 
- *     p = delim + 1;
- *     delim = strchr(p, ' ');
- *     if (!delim)
- *         return -1;
- * 
- *     *delim = '\0';
- *     [> p points to the uri <]
- *     strncpy(reqmeta->uri, p, strlen(p) + 1); */
-    parse_uri(reqmeta->uri, reqmeta->host, reqmeta->path, &reqmeta->port);
-
-    return 1;
-}
-
-int get_header(rio_t *rio_buf, reqmeta_t *reqmeta, io_buf_t *io_buf);
+int parse_request_line(rio_t *rio_buf, http_reqline_t *reqline, io_buf_t *io_buf);
+int get_header(rio_t *rio_buf, io_buf_t *io_buf, int *content_lenp);
 int get_body(rio_t *rio_buf, io_buf_t *io_buf, int content_len);
+int send_reqline(int fd, io_buf_t *iobuf, http_reqline_t *reqline);
+ssize_t read_and_send_headers(int fd, rio_t *rio_buf, io_buf_t *io_buf, int *content_lenp);
+int read_and_send_body(int fd, rio_t *rio_buf, io_buf_t *io_buf, int content_len);
 
-ssize_t send_headers(int fd, rio_t *rio_buf, io_buf_t *io_buf, reqmeta_t *reqmeta);
-int send_body(int fd, rio_t *rio_buf, io_buf_t *io_buf, int content_len);
-
-void clear_reqmeta(reqmeta_t *reqmeta)
-{
-    memset(reqmeta, 0, sizeof(*reqmeta));
-}
+/** Miscellaneous for multi-thread **/
+static int thread_no;
+int create_thread(pthread_t *tidp, pthread_attr_t *attrp, thread_func_t *funcp, void *argp);
+thread_func_t proxy_worker;
 
 /*
  * main - Main routine for the proxy program
  */
 int main(int argc, char **argv)
 {
-    signal(SIGSEGV, handler);
+    signal(SIGSEGV, segv_handler);
     /* ignore SIGPIPE signaled when write to closed socket */
     signal(SIGPIPE, SIG_IGN);
 
@@ -128,119 +119,180 @@ int main(int argc, char **argv)
     }
 
     port = atoi(argv[1]);
-    int listenfd, browserfd, targetfd;
+    int listenfd, browserfd;
     struct sockaddr_in browser_addr;
     socklen_t addrlen = sizeof(browser_addr);
+
+    init_gethostbyname_mutex();
 
     /* open listenning fd */
     listenfd = Open_listenfd(port);
 
-    FILE *flogp = fopen("proxy.log", "a");
-    if (!flogp)
-        unix_error("fopen");
+    /* all threads shared a logger */
+    proxy_logger_t *loggerp = proxy_logger("proxy.log");
+    if (!loggerp) {
+        LOG_ERROR(("create proxy logger fail"));
+        exit(1);
+    }
 
     printf("server: started on port: %d.\n", port);
 
-    /* we only need two buffer */
-    rio_t browser_buf;
-    rio_t target_buf;
-    io_buf_t io_buf;
-    char logstring[MAXLINE];
-    ssize_t nbyte;
-    int rc;
 
-    reqmeta_t reqmeta;
-    memset(&reqmeta, 0, sizeof(reqmeta));
     while (1) {
         browserfd = Accept(listenfd, (struct sockaddr *)&browser_addr, &addrlen);
         DBG(("accept a new connection"));
 
-        Rio_readinitb(&browser_buf, browserfd);
-
-        while (1) {
-            /* parse request line */
-            rc = parse_request_line(&browser_buf, &reqmeta, &io_buf);
-            if (rc == 0) {
-                DBG(("browser socket has been closed"));
-                break;
-            }
-            if (rc == -1)
-                continue;
-
-            /* connect to target server */
-            DBG(("host: %s, port: %d", reqmeta.host, reqmeta.port));
-            targetfd = Open_clientfd(reqmeta.host, reqmeta.port);
-            /* TODO: set the accepted socket to non-blocking  */
-
-
-            /* send HTTP request to target server */
-            /* request line */
-            sprintf(io_buf.linebuf, "%s %s HTTP/1.1\r\n", reqmeta.method, reqmeta.path);
-            DBG(("sent request line: %s, length: %ld", io_buf.linebuf, strlen(io_buf.linebuf)));
-            Rio_writen(targetfd, io_buf.linebuf, strlen(io_buf.linebuf));
-            reqmeta.content_len = 0;
-
-            /* headers */
-            if (send_headers(targetfd, &browser_buf, &io_buf, &reqmeta) == -1 ||
-                send_body(targetfd, &browser_buf, &io_buf, reqmeta.content_len) == -1)
-                break;
-
-            DBG(("forwarded request to target server"));
-
-            /**** send response back to browser *****/
-            Rio_readinitb(&target_buf, targetfd);
-            ssize_t rc;
-            nbyte = 0;
-            reqmeta.content_len = 0;
-
-            if ((rc = send_headers(browserfd, &target_buf, &io_buf, &reqmeta)) == -1)
-                break;
-            nbyte += rc;
-
-            if (!send_body(browserfd, &target_buf, &io_buf, reqmeta.content_len))
-                break;
-            nbyte += reqmeta.content_len;
-
-            /* logging to file */
-            format_log_entry(logstring, &browser_addr, reqmeta.uri, nbyte);
-            write_log(flogp, logstring);
-
-            DBG(("log to file"));
+        if (thread_no > MAXTHREAD) {
+            LOG_ERROR(("thread pool is full, drop new connection"));
+            Close(browserfd);
+            continue;
         }
-        Close(browserfd);
-        Close(targetfd);
 
+        pthread_t tid;
+        thread_context_t *thread_ctxp;
+        thread_ctxp = malloc(sizeof(thread_context_t));
+        if (!thread_ctxp) {
+            LOG_ERROR(("error allocating memory for thread context"));
+            Close(browserfd);
+            continue;
+        }
+
+        thread_ctxp->connfd = browserfd;
+        thread_ctxp->conn_addr = browser_addr;
+        thread_ctxp->tid = thread_no++;
+        thread_ctxp->logger = loggerp;
+
+        /* if create thread fail, do cleanning stuff, skip the connection */
+        if (!create_thread(&tid, NULL, proxy_worker, (void*)thread_ctxp)) {
+            Close(browserfd);
+            free_thread_context(thread_ctxp);
+            continue;
+        }
     }
 
     Close(listenfd);
-    fclose(flogp);
+    free_proxy_logger(loggerp);
     printf("server exits\n");
     exit(0);
 }
 
+
+/**
+ * worker thread logic
+ *
+ */
+void *proxy_worker(void* arg)
+{
+    Pthread_detach(Pthread_self());
+    thread_context_t *thread_ctxp = (thread_context_t*)arg;
+    /* states on the thread stack */
+    rio_t rbuf;
+    io_buf_t iobuf;
+    char logstring[MAXLINE];
+    int remotefd, rc, content_len;
+    ssize_t nbyte = 0;
+    http_reqline_t reqline;
+    Rio_readinitb(&rbuf, thread_ctxp->connfd);
+
+    while (1) {
+        if ((rc = parse_request_line(&rbuf, &reqline, &iobuf)) == 0)
+            goto terminate;
+
+        /* connect to remote server */
+        remotefd = Open_clientfd(reqline.host, reqline.port);
+        /* send request line to remote server */
+        if (!send_reqline(remotefd, &iobuf, &reqline))
+            goto terminate;
+
+        /* send request to remote */
+        content_len = 0;
+        if (read_and_send_headers(remotefd, &rbuf, &iobuf, &content_len) == 0 ||
+                read_and_send_body(remotefd, &rbuf, &iobuf, content_len) == 0) {
+            LOG_ERROR(("send headers or body to remote host fail"));
+            goto terminate;
+        }
+
+        Rio_readinitb(&rbuf, remotefd);
+
+        /* send response to browser */
+        content_len = 0;
+        if ((rc = read_and_send_headers(thread_ctxp->connfd, &rbuf,
+                        &iobuf, &content_len)) == 0) {
+            LOG_ERROR(("send headers to browser fail"));
+            goto terminate;
+        }
+        nbyte += rc;
+        if ((rc = read_and_send_body(thread_ctxp->connfd, &rbuf, &iobuf,
+                        content_len)) == 0) {
+            LOG_ERROR(("send body to browser fail"));
+            goto terminate;
+        }
+        nbyte += rc;
+
+        format_log_entry(logstring, &thread_ctxp->conn_addr, reqline.uri, nbyte);
+        dolog(thread_ctxp->logger, logstring);
+    }
+
+terminate:
+    /* close client connection and remote connection */
+    Close(thread_ctxp->connfd);
+    Close(remotefd);
+    /* reclaim memory */
+    free_thread_context(thread_ctxp);
+
+    return 0;
+}
+
+
+/**
+ * 解析HTTP Request的第一行，获取目的server的host和port
+ */
+int parse_request_line(rio_t *rio_buf, http_reqline_t *reqline, io_buf_t *io_buf)
+{
+    if (Rio_readlineb(rio_buf, io_buf->linebuf, MAXLINE) == 0 ||
+        strstr(io_buf->linebuf, "HTTP") == NULL)
+        return 0;
+
+    sscanf(io_buf->linebuf, "%s %s %s", reqline->method, reqline->uri, reqline->versionCRLF);
+    parse_uri(reqline->uri, reqline->host, reqline->path, &reqline->port);
+
+    return 1;
+}
+
 /**
  *
- * @return  number of byte sent
- *          -1 for error or EOF
+ * @return 0 for error
  */
-ssize_t send_headers(int fd, rio_t *rio_buf, io_buf_t *io_buf, reqmeta_t *reqmeta)
+int send_reqline(int fd, io_buf_t *iobuf, http_reqline_t *reqline)
+{
+    sprintf(iobuf->linebuf, "%s %s %s", reqline->method, reqline->path, reqline->versionCRLF);
+    if (rio_writen(fd, iobuf->linebuf, strlen(iobuf->linebuf)) < 0) {
+        print_err(errno, "rio_writen");
+        return 0;
+    }
+    return 1;
+}
+
+/**
+ * read headers from rio_buf and wirte to fd
+ * @return  number of byte sent
+ *          0 for error or EOF
+ */
+ssize_t read_and_send_headers(int fd, rio_t *rio_buf, io_buf_t *io_buf, int *content_lenp)
 {
     ssize_t nbyte = 0;
     int rc;
 
-    /* add proxy header */
-    /* sprintf(io_buf->linebuf, "%s: %s\r\n", "X-SIYUAN-PROXY", "Siyuan Wang");
-     * if ((rio_writen(fd, io_buf->linebuf, strlen(io_buf->linebuf)) < 0))
-     *     return -1; */
-
     do {
-        if ((rc = get_header(rio_buf, reqmeta, io_buf)) <= 0) /* EOF */
-            return -1;
+        if ((rc = get_header(rio_buf, io_buf, content_lenp)) <= 0) /* EOF */
+            return 0;
 
-        if ((rc = rio_writen(fd, io_buf->linebuf, strlen(io_buf->linebuf))) < 0) /* error */
-            return -1;
+        if ((rc = rio_writen(fd, io_buf->linebuf, strlen(io_buf->linebuf))) < 0) { /* error */
+            print_err(errno, "rio_writen");
+            return 0;
+        }
 
-        nbyte += strlen(io_buf->linebuf);
+        nbyte += rc;
     } while (strncmp(CRLF, io_buf->linebuf, 2) != 0);
 
     return nbyte;
@@ -250,7 +302,7 @@ ssize_t send_headers(int fd, rio_t *rio_buf, io_buf_t *io_buf, reqmeta_t *reqmet
  * @return 1 for successfully
  *         0 for error
  */
-int send_body(int fd, rio_t *rio_buf, io_buf_t *io_buf, int content_len)
+int read_and_send_body(int fd, rio_t *rio_buf, io_buf_t *io_buf, int content_len)
 {
     ssize_t nleft = content_len;
     int rc;
@@ -268,11 +320,84 @@ int send_body(int fd, rio_t *rio_buf, io_buf_t *io_buf, int content_len)
     return 1;
 }
 
-void write_log(FILE *fp, char *logstring)
+proxy_logger_t *proxy_logger(const char *filename)
 {
-    fprintf(fp, "%s\n", logstring);
-    fflush(fp);
+    proxy_logger_t *loggerp = malloc(sizeof(proxy_logger_t));
+    if (!loggerp) {
+        LOG_ERROR(("allocate memory for proxy logger fail"));
+        return NULL;
+    }
+
+
+    FILE *fp = fopen(filename, "a");
+    if (!fp) {
+        print_err(errno, "open proxy log fail");
+        fclose(fp);
+        exit(1);
+    }
+
+    loggerp->fp = fp;
+    /* OSX好像没有实现这个函数 */
+    if (sem_init(&loggerp->mutex, 0, 1) < 0) {
+        print_err(errno, "sem_init");
+        return NULL;
+    }
+
+    return loggerp;
 }
+
+void dolog(proxy_logger_t *loggerp, char *logstring)
+{
+    sem_wait(&loggerp->mutex);
+    fprintf(loggerp->fp, "%s\n", logstring);
+    fflush(loggerp->fp);
+    sem_post(&loggerp->mutex);
+}
+
+void free_proxy_logger(proxy_logger_t *loggerp)
+{
+    if (NULL == loggerp)
+        return;
+
+    fclose(loggerp->fp);
+    free(loggerp);
+}
+
+void free_thread_context(thread_context_t *thread_ctxp)
+{
+    if (NULL == thread_ctxp)
+        return;
+
+    free(thread_ctxp);
+    thread_no--;
+}
+
+void print_err(int err, char *msg)
+{
+    char errbuf[256];
+    strerror_r(err, errbuf, 256);
+    fprintf(stderr, "%s: %s\n", msg, errbuf);
+}
+
+
+/**
+ * wrapper for pthread_create()
+ * @return 0 for error
+ */
+int create_thread(pthread_t *tidp, pthread_attr_t *attrp, thread_func_t *funcp, void *argp)
+{
+    int rc;
+    if ((rc = pthread_create(tidp, attrp, funcp, argp)) != 0) {
+        print_err(rc, "pthread_create error");
+        return 0;
+    }
+
+    return 1;
+}
+
+
+
+/** HTTP message related functions **/
 
 /**
  * read a header in io_buf.linebuf
@@ -280,7 +405,7 @@ void write_log(FILE *fp, char *logstring)
  *         0 for EOF or error
  *         1 for successfully read a header
  */
-int get_header(rio_t *rio_buf, reqmeta_t *reqmeta, io_buf_t *io_buf)
+int get_header(rio_t *rio_buf, io_buf_t *io_buf, int *content_lenp)
 {
     /* parse all headers */
     char *delim;
@@ -290,9 +415,11 @@ int get_header(rio_t *rio_buf, reqmeta_t *reqmeta, io_buf_t *io_buf)
 
     /* encounter "Content-Length" means there is payload in the request */
     if (strncmp(CONTENT_LENGTH, io_buf->linebuf, strlen(CONTENT_LENGTH)) == 0) {
+        assert (*content_lenp == 0);
         DBG(("%s", io_buf->linebuf));
         delim = strchr(io_buf->linebuf, ':');
-        reqmeta->content_len = atoi(delim + 1);
+        assert (delim != NULL);
+        *content_lenp = atoi(delim + 1);
     }
 
     return 1;
